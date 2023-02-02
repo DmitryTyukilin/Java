c3b2aed82d8f62dd026381d35eaac7121db346b8
Dmitry <tyukilin555@gmail.com>
2023-02-02 21:50:41 +0300

    Initial commit

--- Java/src/com/tyukilin/java/hometask/cs/task2.java
+++ Java/src/com/tyukilin/java/hometask/cs/task2.java
@@ -13,9 +13,14 @@ package com.tyukilin.java.hometask.cs;
 public class task2 {
     public static void main(String[] args) {
         double operand1 = 135.0;
-        double operand2 = 10.0;
-        char operation = '%';
-        System.out.println(calculationValue(operand1, operand2, operation));
+        double operand2 = 3;
+        char operation = '/';
+        if (operand2 != 0) {
+            System.out.println(calculationValue(operand1, operand2, operation));
+        }
+        else {
+            System.out.println("incorrect value");
+        }
     }
     public static double calculationValue(double operand1, double operand2, char operation) {
         double result = 0;
